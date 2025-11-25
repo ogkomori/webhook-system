13a0f0dfe7cf3ca5e0b82448da09e5a81fc37fd4
Tega Komori <majorogkomori@gmail.com>
2025-11-25 01:13:52 +0000

    Add RabbitMQ configuration

---
+++ worker/src/main/java/com/komori/webhook/config/RabbitConfig.java
@@ -0,0 +1,13 @@
+package com.komori.webhook.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+    @Bean
+    public Queue eventsQueue() {
+        return new Queue("events", true);
+    }
+}
---
+++ worker/src/main/java/com/komori/webhook/service/EventConsumer.java
@@ -0,0 +1,15 @@
+package com.komori.webhook.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class EventConsumer {
+    @RabbitListener(queues = "events")
+    public void consume(String event) {
+        // just to confirm consumption
+        log.info("Event received: {}", event);
+    }
+}
