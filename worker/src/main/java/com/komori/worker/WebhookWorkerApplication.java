ccf890fa6cf0955da4824b734b2a0549edf423f3
Tega Komori <majorogkomori@gmail.com>
2025-12-09 10:54:45 +0000

    Add PersistenceConfig for JPA repository and entity scanning

--- worker/src/main/java/com/komori/worker/WebhookWorkerApplication.java
+++ worker/src/main/java/com/komori/worker/WebhookWorkerApplication.java
@@ -2,15 +2,12 @@ package com.komori.worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.komori.persistence.repository")
-@EntityScan(basePackages = "com.komori.persistence.entity")
 public class WebhookWorkerApplication {
 
     static void main(String[] args) {
         SpringApplication.run(WebhookWorkerApplication.class, args);
     }
+
 }
---
+++ worker/src/main/java/com/komori/worker/config/PersistenceConfig.java
@@ -0,0 +1,11 @@
+package com.komori.worker.config;
+
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.komori.persistence.repository")
+@EntityScan(basePackages = "com.komori.persistence.entity")
+public class PersistenceConfig {
+}
